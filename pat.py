efb61f02cfa537491d7ad3ca347e8b3e9685c651
Eunice21-Afia <tawiaheunice51@gmail.com>
2024-12-29 20:32:31 -0500

    Initial Commit

---
+++ pat.py
@@ -0,0 +1,10 @@
+# name = input("Enter your name: ")
+
+# age = int (input ("Enter your age 4 years ago: "))
+
+# age_now = 4 + age
+
+# print ("User Name is",name,".Was " , age , " 4 years ago and is", age_now, "now." )
+
+user = "He \C'told me to' add a \nnew line and \tindent my text but i said i want a \nnew line and \breduce the indent. play \a as well"
+print (user)
